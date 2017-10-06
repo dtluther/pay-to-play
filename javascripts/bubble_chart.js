9d5488e2f4eb2f2c4125a092f18dc7d310c24ce2
Dillon <dillontluther@gmail.com>
2017-10-06 09:40:46 -0700

    cleaning up folders

--- javascripts/bubble_chart.js
+++
@@ -1,9 +0,0 @@
-const bubbleChart = function () {
-  const [ width, height ] = [ 600, 400 ];
-
-  function chart(selection) {
-    // later
-  }
-
-  return chart;
-};
--- javascripts/root.js
+++
@@ -1,1 +0,0 @@
-import athletesJson from './athletes.js';
